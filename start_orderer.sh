e1ed3df461298cacb1423868e2210b277089e0aa
pankaj <pankajcheema1990@gmail.com>
2018-06-25 14:08:00 +0530

    script updated

--- start_orderer.sh
+++ start_orderer.sh
@@ -1,15 +1,15 @@
 
 
 ORDERER_GENERAL_LOGLEVEL=debug \
-ORDERER_GENERAL_LISTENADDRESS=firstorderer.debutinfotech.com \
+ORDERER_GENERAL_LISTENADDRESS=secondorder.debutinfotech.com \
 ORDERER_GENERAL_GENESISMETHOD=file \
-ORDERER_GENERAL_GENESISFILE=$HOME/debutNetwork/channel-artifacts/genesis.block \
+ORDERER_GENERAL_GENESISFILE=$PWD/channel-artifacts/genesis.block \
 ORDERER_GENERAL_LOCALMSPID=debutOrdererMSP \
-ORDERER_GENERAL_LOCALMSPDIR=$HOME/debutNetwork/crypto-config/ordererOrganizations/debutinfotech.com/orderers/firstorderer.debutinfotech.com/msp \
+ORDERER_GENERAL_LOCALMSPDIR=$PWD/crypto-config/ordererOrganizations/debutinfotech.com/orderers/secondorder.debutinfotech.com/msp \
 ORDERER_GENERAL_TLS_ENABLED=false \
-ORDERER_GENERAL_TLS_PRIVATEKEY=$HOME/debutNetwork/crypto-config/ordererOrganizations/debutinfotech.com/orderers/firstorderer.debutinfotech.com/tls/server.key \
-ORDERER_GENERAL_TLS_CERTIFICATE=$HOME/debutNetwork/crypto-config/ordererOrganizations/debutinfotech.com/orderers/firstorderer.debutinfotech.com/tls/server.crt \
-ORDERER_GENERAL_TLS_ROOTCAS=[$HOME/debutNetwork/crypto-config/ordererOrganizations/debutinfotech.com/orderers/firstorderer.debutinfotech.com/tls/ca.crt] \
+ORDERER_GENERAL_TLS_PRIVATEKEY=$PWD/crypto-config/ordererOrganizations/debutinfotech.com/orderers/secondorder.debutinfotech.com/tls/server.key \
+ORDERER_GENERAL_TLS_CERTIFICATE=$PWD/crypto-config/ordererOrganizations/debutinfotech.com/orderers/secondorder.debutinfotech.com/tls/server.crt \
+ORDERER_GENERAL_TLS_ROOTCAS=[$PWD/crypto-config/ordererOrganizations/debutinfotech.com/orderers/secondorder.debutinfotech.com/tls/ca.crt] \
 CONFIGTX_ORDERER_BATCHTIMEOUT=1s \
 CONFIGTX_ORDERER_ORDERERTYPE=kafka \
 CONFIGTX_ORDERER_KAFKA_BROKERS=[kafka0:9092,kafka1:9092,kafka2:9092,kafka3:9092] \
